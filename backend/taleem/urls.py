36a3c962cc848e8edc2806606f4d418b5ca94049
Shahzaib Akash <shahzaibakash@gmail.com>
2020-06-18 06:04:25 +0500

    remove example app

--- backend/taleem/urls.py
+++ backend/taleem/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("", lambda request : redirect("/exampleapp/")),
     path("admin/", admin.site.urls, name="admin"),
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
-    path("exampleapp/", include("exampleapp.urls"), name="exampleapp"),
 ]
